60810dc3117362b983af5f5c8f36632ca1c6903e
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-03-25 09:24:22 +0000

    Added new one

--- tools/handle_2.py
+++ tools/handle_2.py
@@ -2,72 +2,57 @@ from typing import Any, Dict, List
 
 MODIFIERS = ["=", "+", "-"]
 
-def set_types(statement: str) -> List[Any]:
-    to_return: List = []
+def set_type(item: Any, variables: Dict[str, Any]) -> Any:
+	var_keys = list(vars.keys())
+	if item in var_keys:
+		print(f" {item}  is in  {var_keys}")
+		print(f"{variables[item]}")
+		print(f"{str(variables[item]).isdigit()}")
+		item = variables[item]
+	else:
+		print(f" {item}  is not in  {var_keys}")
 
 
-    to_add = ""
-    for character in statement:
-        if character in MODIFIERS:
-            if to_add.isdigit():
-                to_add = int(to_add)
-            elif to_add.startswith("\"") and to_add.endswith("\""):
-                print(f"{to_add} starts with \"")
-                to_add = to_add[1:-1]
-            else:
-                print(f"'{to_add}' doesn't start with \"")
-                print(f"{to_add} is not valid type")
-
-            to_return.append(to_add)
-            to_return.append(character)
-            to_add = ""
-            continue
-        
-        to_add += character
-    to_return.append(to_add)
-    
-    return to_return
+	if str(item).isdigit():
+		item = int(item)
+	elif item.startswith('"') and item.endswith('"'):
+		print(f"{item} starts with \"")
+		item = item[1:-1]
+	else:
+		print(f"{item} is not valid type")
+	return item
 
 
-def handle(statement: str, variables: Dict[str, Any]) -> Any:
-    variable_keys = list(variables.keys())
-    statement_split = statement.split(" ")
+def handle_statement(statement: str, variables: Dict[str, Any]) -> List[Any]:
+	to_return: List = []
 
-    current = None
+	var_keys = list(variables.keys())
+	
+	
+	to_add = ""
+	for character in statement:
+		if character in MODIFIERS:
+			to_add = set_type(to_add, variables)
+			to_return.append(to_add)
+			to_return.append(character)
+			to_add = ""
+			continue
+		elif character == " ":
+			continue
+		to_add += character
 
-    modifier = "="
-
-    for item in statement_split:
-        if item in variable_keys:
-            alter = variables[item]
-        elif item in MODIFIERS:
-            modifier = item
-            alter = None
-        else:
-            alter = item
-
-        
-        match modifier:
-            case "=":
-                current = item
-            case "+":
-                current += item
-
-
-        print(f"  {str(item).ljust(10)} // Current: {str(current).ljust(10)} // Alter: {str(alter).ljust(10)}")
-
-
-    return current
-
+	to_return.append(set_type(to_add, variables))
+		
+	return to_return
+		
+	
 
 if __name__ == "__main__":
-    # vars = {
-    #     "testi_1": 3,
-    #     "testi_2": 2,
-    #     "tests_1": "hi"
-    # }
-    # statement = "1 + test-i_1"
-
-    # print(handle(statement, vars))
-    print(set_types("1 + testi_2 + testi_1"))
-    print(set_types("\"hi\" + tests_1"))
+	vars = {
+	    "testi_1": 3,
+	    "testi_2": 2,
+	    "tests_1": "hi"
+	}
+	
+	print(handle_statement("1 + testi_2 + testi_1", vars))
+	print(handle_statement("\"hi\" + tests_1", vars))
